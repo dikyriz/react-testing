15ea4cf1accb5133d8938be5d72f6b461576adc5
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-02-03 08:44:26 +0700

    add new file and folder

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const root = document.querySelector('#root');
+
+function tick(){
+    const element = (
+        <div>
+            <h1>Jam Sekarang</h1>
+            {new Date().toLocaleTimeString()}
+        </div>
+    )
+
+    ReactDOM.render(element, root);
+}
+
+tick();
+
+setInterval(function() {
+    tick();
+}, 1000);
