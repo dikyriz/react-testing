78670c82185fa98c1ae2661ab72714c6eb9db6de
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-02-07 20:33:42 +0700

    aturan penamaan file di Javascript ES6

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const root = document.querySelector('#root');
 
     function App () {
-        const [count, updateCount] = React.useState(0);
+        const [count, setCount] = React.useState(0);
 
         return (
             <>
                 <button onClick={function () {
-                    updateCount(count - 1);
+                    setCount(count - 1);
                 }}>-</button>
                 <span>{count}</span>
                 <button onClick={function () {
-                    updateCount(count + 1);
+                    setCount(count + 1);
                 }}>+</button>
             </>
         );
