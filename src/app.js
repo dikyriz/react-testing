a4f5fcb630e84af7e7a07a9f26b6799b7784ec50
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-02-08 11:07:28 +0700

    penggunaan React list

--- src/app.js
+++ src/app.js
@@ -1,20 +1,17 @@
 const root = document.querySelector('#root');
 
     function App () {
-        const [login, setLogin] = React.useState(false);
-        const judulRef = React.useRef(null);
-        
-        React.useEffect(function () {
-            setTimeout(function()  {
-                judulRef.current.textContent = 'Aplikasi';
-                
-            }, 1000);
-            // console.log(judul);
-        });
+        const fruits = ['Apple', 'Orange', 'Grape', 'Lengkeng'];
 
         return (
             <>
-                <h1 ref={judulRef}>Application</h1>
+
+                <ul>
+                {fruits.map(function (fruit) {
+                    return <li key={fruit}>{fruit}</li>;
+                    
+                })}
+                </ul>
             </>
         );
     }
