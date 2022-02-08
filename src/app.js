c0184a2e03490e4ac9be78954157293fed92f26f
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-02-08 14:30:53 +0700

    Penggunaan React list pada data array API

--- src/app.js
+++ src/app.js
@@ -1,19 +1,34 @@
 const root = document.querySelector('#root');
 
     function App () {
+        const [news, setNews] = React.useState([]);
+        const [loading, setLoading] = React.useState(true);
+
         React.useEffect(function () {
             async function getData () {
                 const request = await fetch('https://api.spaceflightnewsapi.net/v3/blogs');
 
                 const response = await request.json();
 
-                console.log(response);
+                setNews(response);
+                setLoading(false);
             }
             getData();
         }, []);
         
 
-        return <h1>Data Fetch</h1>;
+        return (
+        <>
+            <h1>Data Fetch</h1>
+            {loading ? (<i>Loading data ....</i>) : (
+            <ul>
+            {news.map(function (item) {
+                return <li key={item.id}>{item.title}</li>;
+            })} 
+            </ul>
+            )}
+        </> 
+        );
     }
 
     ReactDOM.render(<App />, root);
