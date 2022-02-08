2f0e5c30ba97a32ddef95d69de1e1c9354942b4b
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-02-08 20:52:57 +0700

    penambahan fungsi edit, update, cancel, message dan checkbox

--- public/app.js
+++ public/app.js
@@ -3,18 +3,47 @@ const root = document.querySelector('#root');
 function App() {
   const [activity, setActivity] = React.useState('');
   const [todos, setTodos] = React.useState([]);
+  const [edit, setEdit] = React.useState({});
+  const [message, setMessage] = React.useState('');
 
   function generateId() {
     return Date.now();
   }
 
-  function addTodoList(event) {
+  function saveTodoList(event) {
     event.preventDefault();
+
+    if (!activity) {
+      return setMessage('Aktivitas tidak boleh kosong');
+    }
+
+    setMessage('');
+
+    if (edit.id) {
+      const updateTodo = { ...edit,
+        activity,
+        done: false
+      };
+      const editTodoIndex = todos.findIndex(function (todo) {
+        return todo.id === edit.id;
+      });
+      const updatedTodos = [...todos];
+      updatedTodos[editTodoIndex] = updateTodo;
+      setTodos(updatedTodos);
+      return cancelTodoList();
+    }
+
     setTodos([...todos, {
       id: generateId(),
-      activity
+      activity,
+      done: false
     }]);
     setActivity('');
+  }
+
+  function editTodoList(todo) {
+    setActivity(todo.activity);
+    setEdit(todo);
   }
 
   function removeTodoList(todoId) {
@@ -22,26 +51,56 @@ function App() {
       return todo.id !== todoId;
     });
     setTodos(filteredTodos);
+    if (edit.id) cancelTodoList();
   }
 
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Simple Todo List"), /*#__PURE__*/React.createElement("form", {
-    onSubmit: addTodoList
+  function cancelTodoList() {
+    setEdit({});
+    setActivity('');
+  }
+
+  function doneTodoList(todo) {
+    const updateTodo = { ...todo,
+      done: todo.done ? false : true
+    };
+    const editTodoIndex = todos.findIndex(function (currentTodo) {
+      return currentTodo.id === todo.id;
+    });
+    const updatedTodos = [...todos];
+    updatedTodos[editTodoIndex] = updateTodo;
+    setTodos(updatedTodos);
+  }
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Simple Todo List"), message && /*#__PURE__*/React.createElement("div", {
+    style: {
+      color: 'red'
+    }
+  }, message), /*#__PURE__*/React.createElement("form", {
+    onSubmit: saveTodoList
   }, /*#__PURE__*/React.createElement("input", {
     type: "text",
-    placeholder: "Nama Aktifitas",
+    placeholder: "Nama Aktivitas",
     value: activity,
     onChange: function (event) {
       setActivity(event.target.value);
     }
   }), /*#__PURE__*/React.createElement("button", {
     type: "submit"
-  }, "Tambah")), /*#__PURE__*/React.createElement("ul", null, todos.map(function (todo) {
+  }, edit.id ? "Simpan" : "Tambah"), edit.id && /*#__PURE__*/React.createElement("button", {
+    onClick: cancelTodoList
+  }, "Cancel")), todos.length > 0 ? /*#__PURE__*/React.createElement("ul", null, todos.map(function (todo) {
     return /*#__PURE__*/React.createElement("li", {
       key: todo.id
-    }, todo.activity, /*#__PURE__*/React.createElement("button", {
+    }, /*#__PURE__*/React.createElement("input", {
+      type: "checkbox",
+      checked: todo.done,
+      onChange: doneTodoList.bind(this, todo)
+    }), todo.activity, " ", todo.done ? '(Selesai)' : '(Belum Selesai)', /*#__PURE__*/React.createElement("button", {
+      onClick: editTodoList.bind(this, todo)
+    }, "Edit"), /*#__PURE__*/React.createElement("button", {
       onClick: removeTodoList.bind(this, todo.id)
     }, "Hapus"));
-  })));
+  })) : /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("i", null, "Belum Ada Data Aktivitas")));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -3,22 +3,60 @@ const root = document.querySelector('#root');
     function App () {
         const [activity, setActivity] = React.useState('');
         const [todos, setTodos] = React.useState([]);
+        const [edit, setEdit] = React.useState({});
+        const [message, setMessage] = React.useState('');
         
         function generateId () {
             return Date.now();
         }
         
-        function addTodoList (event) {
+        function saveTodoList (event) {
             event.preventDefault();
+
+            if(!activity) {
+               return setMessage('Aktivitas tidak boleh kosong');
+            }
+
+            setMessage('');
+
+            if(edit.id) {
+                const updateTodo = {
+                    ... edit,
+                    activity,
+                    done: false
+                };
+
+                const editTodoIndex = todos.findIndex(function (todo) {
+                    return todo.id === edit.id;
+                }) ;
+                
+                const updatedTodos = [... todos];
+
+                updatedTodos[editTodoIndex] = updateTodo;
+
+                setTodos(updatedTodos);
+
+                return cancelTodoList();
+            }
+
+            
+
             setTodos([
                 ... todos, 
                 {
                     id: generateId(),
-                    activity
+                    activity,
+                    done: false
                 }
             ]);
+            
             setActivity('');
 
+        }
+
+        function editTodoList (todo) {
+            setActivity(todo.activity);
+            setEdit(todo);
         }
 
         function removeTodoList (todoId) {
@@ -26,29 +64,65 @@ const root = document.querySelector('#root');
                 return todo.id !== todoId;
             });
             setTodos(filteredTodos);
+
+            if(edit.id) cancelTodoList();
+        }
+
+        function cancelTodoList () {
+            setEdit({});
+            setActivity('');
+        }
+
+        function doneTodoList(todo) {
+            const updateTodo = {
+                ... todo,
+                done: todo.done ? false : true
+            };
+
+            const editTodoIndex = todos.findIndex(function (currentTodo) {
+                    return currentTodo.id === todo.id;
+                }) ;
+                
+                const updatedTodos = [... todos];
+
+                updatedTodos[editTodoIndex] = updateTodo;
+
+                setTodos(updatedTodos);
+            
         }
 
         return(
             <>
                 <h1>Simple Todo List</h1>
-                <form onSubmit={addTodoList}>
-                    <input type="text" placeholder="Nama Aktifitas" value={activity} onChange={function (event) {
+                {message && <div style={{ color: 'red' }}>{message}</div>}
+                <form onSubmit={saveTodoList}>
+                    <input type="text" placeholder="Nama Aktivitas" value={activity} onChange={function (event) {
                         setActivity(event.target.value);
                     }}/>
-                    <button type="submit">Tambah</button>
+                    <button type="submit">
+                        {edit.id ? "Simpan" : "Tambah"}
+                    </button>
+                    {edit.id && <button onClick={cancelTodoList}>Cancel</button>}
+                    
                 </form>
-                <ul>
+                {todos.length > 0 ? (
+                    <ul>
                     {todos.map(function (todo) {
                         return (
                                 <li key={todo.id}>
-                                {todo.activity}
+                                <input type="checkbox" checked={todo.done} onChange={doneTodoList.bind(this, todo)}/>
+                                {todo.activity} {todo.done ? '(Selesai)' : '(Belum Selesai)'}
+                                <button onClick={editTodoList.bind(this, todo)}>Edit</button>
                                 <button onClick={removeTodoList.bind(this, todo.id)}>Hapus</button>
                                 </li>
                             
                             );
                     })}
                     
-                </ul>
+                </ul> )
+                : ( <p><i>Belum Ada Data Aktivitas</i></p> )
+                }
+                
             </>
         );
     }
