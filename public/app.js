4bb8c7b6e83bd09867d04ecb413901f8d12bcba7
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-02-08 10:37:30 +0700

    penggunaan ternary operator tanpa menggunakan kondisi else

--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
 const root = document.querySelector('#root');
 
 function App() {
-  const [login, setLogin] = React.useState(false);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Application"), /*#__PURE__*/React.createElement("p", null, login ? /*#__PURE__*/React.createElement("b", null, "Sudah Login") : /*#__PURE__*/React.createElement("i", null, "Belum Login")), /*#__PURE__*/React.createElement("button", {
+  const [login, setLogin] = React.useState(false); // console.log(login);
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Application"), /*#__PURE__*/React.createElement("p", null, !login && /*#__PURE__*/React.createElement("b", null, "Sudah Login")), /*#__PURE__*/React.createElement("button", {
     onClick: function () {
       setLogin(true);
     }
--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ const root = document.querySelector('#root');
 
     function App () {
         const [login, setLogin] = React.useState(false);
-        
+        // console.log(login);
 
         return (
             <>
                 <h1>Application</h1>
-                <p>{login ? <b>Sudah Login</b> :  <i>Belum Login</i>}</p>
+                <p>{!login && <b>Sudah Login</b>}</p>
                 <button onClick={function () {
                     setLogin(true);
                 }}>Login</button>
